f2620a05fb1a62b1c7c6dba6bce8d434ca87e909
Yoqub <yyboboraximov@gmail.com>
2022-03-30 10:45:19 +0500

    new_project

---
+++ src/main/java/com/example/dars_vazifa/DarsVazifaApplication.java
@@ -0,0 +1,13 @@
+package com.example.dars_vazifa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DarsVazifaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DarsVazifaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/dars_vazifa/DarsVazifaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dars_vazifa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DarsVazifaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
